c973c1465901675c886b131a3f69a334ab68a69a
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-05-23 13:16:17 +0200

    feat: add animation to open navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { styled } from 'styled-components';
 import { Container } from '../styles/GlobalComponents';
 import { theme } from '../styles/theme';
+import ArrowUpCircle from './UI/ArrowUpCircle';
+import { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
 
 const HeaderWrapper = styled.header`
   position: fixed;
@@ -21,17 +24,45 @@ const HeaderWrapper = styled.header`
     }
   }
 
-  .nav {
-    width: 25px;
-    @media (max-width: ${theme.breakpoints.phone}) {
+  .nav-toggle {
+    cursor: pointer;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @media (max-width: ${theme.breakpoints.tablet}) {
+      margin-right: 24px;
+    }
+
+    @media (max-width: ${theme.breakpoints.tablet}) {
+      margin-right: 16px;
       width: 20px;
     }
-    span {
-      margin-bottom: 0.3rem;
-      background-color: ${theme.color.black};
-      height: 2px;
-      width: 100%;
-      display: block;
+    .hamburger-menu {
+      width: 25px;
+
+      span {
+        margin-bottom: 0.3rem;
+        background-color: ${theme.color.black};
+        height: 2px;
+        width: 100%;
+        display: block;
+      }
+    }
+
+    .hamburger-menu-close {
+      position: absolute;
+      display: none;
+      svg {
+        @media (max-width: ${theme.breakpoints.tablet}) {
+          width: 56px;
+        }
+
+        @media (max-width: ${theme.breakpoints.tablet}) {
+          width: 48px;
+        }
+      }
     }
   }
 `;
@@ -41,7 +72,29 @@ const Row = styled.div`
   align-items: center;
 `;
 
+const tl = gsap.timeline();
+const mm = gsap.matchMedia();
+
 const Header = () => {
+  const [menuState, setMenuState] = useState({ menuOpened: false });
+
+  useEffect(() => {
+    if (menuState.menuOpened) {
+      // Run animations to open menu
+      gsap.to('nav', { css: { display: 'block' } });
+      gsap.to('body', { css: { overflow: 'hidden' } });
+
+      mm.add('(max-width: 654px)', () => {
+        tl.to('.App', { duration: 1, y: '70svh', ease: 'expo.inOut' });
+      });
+      mm.add('(min-width: 655px)', () => {
+        tl.to('.App', { duration: 1, y: '50svh', ease: 'expo.inOut' });
+      });
+    } else {
+      // Run close menu animations
+    }
+  }, [menuState.menuOpened]);
+
   return (
     <HeaderWrapper>
       <Container>
@@ -49,9 +102,20 @@ const Header = () => {
           <div className="logo">
             <a href="/">AGENCY.</a>
           </div>
-          <div className="nav">
-            <span></span>
-            <span></span>
+          <div className="nav-toggle">
+            <div
+              onClick={() => setMenuState({ menuOpened: true })}
+              className="hamburger-menu"
+            >
+              <span></span>
+              <span></span>
+            </div>
+            <div
+              onClick={() => setMenuState({ menuOpened: false })}
+              className="hamburger-menu-close"
+            >
+              <ArrowUpCircle />
+            </div>
           </div>
         </Row>
       </Container>
